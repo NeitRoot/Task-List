4563ca4043fc20cbfcdeaebc854b57ea42e403d6
NeitRoot <natya.vizcarra@gmail.com>
2022-06-08 19:58:54 -0300

    Se agregó mock de tareas e interface de las mismas

---
+++ src/app/mock-task.ts
@@ -0,0 +1,30 @@
+import {Task}  from "./Task"
+
+export const TASK: Task[] = [
+    {
+id: 1,
+text: "Terminar primer modulo de Angular",
+day: 'Agosto 10 a las 12:00',
+reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer compras",
+        day: 'Julio 7 a las 17:00',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Investigas sobre boostrap",
+        day: 'Junio 8 a las 19:00',
+        reminder: false
+     },
+     {
+        id: 4,
+        text: "Leer mails",
+        day: 'Agosto 5 a las 21:00',
+        reminder: false
+     } 
+
+
+]
